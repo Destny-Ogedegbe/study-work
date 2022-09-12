e7e14f2fb028025ec592bcdc8f0509e78cebd829
Destny-Ogedegbe <ogesdestiny@gmail.com>
2022-09-11 20:44:54 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - entry point
- * Return: Always 1 (success)
+ * main - like a room where programs work
+ * printf - to output a text, expression etc
+ * Return: return 1 (that is return nothing)
  */
-int main(void)
-{
-	write(2,
-			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n". 59);
 
-	return (1);
+int void(main)
+{
+char e* = "and that piece of art is useful\" - Dora Korpar. 2015-10-19\n";
+fprintf(stderr, "%s", e);
+return (1);
 }
+
