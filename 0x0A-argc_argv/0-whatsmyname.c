60b9a450a7d0b287a52b6574841a71a24f92d62c
Destny-Ogedegbe <ogesdestiny@gmail.com>
2022-09-30 18:34:05 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,19 @@
-#include "main.h"
 #include <stdio.h>
-#define UNUSED(x) (void)(x)
+
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
+ * main - write a program that prints its name, followed y a new line.
+ * If you rename the program, it will print the new name without having to
+ * compile it again
+ * You should not remove the path before the name of the program
+ *
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ *
+ * Return: This returns to zero
  */
 int main(int argc, char *argv[])
 {
-	UNUSED(argc);
 	printf("%s\n", argv[0]);
-	return (0);
+	void(argc)
+		return (0);
 }
