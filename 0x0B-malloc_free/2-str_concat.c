672cd1dacd915290de4b90518ee97c7e4fe86db8
Destny-Ogedegbe <ogesdestiny@gmail.com>
2022-10-05 20:16:05 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - function entry point
+ * @s1: first string
+ * @s2: second string
+ * Desc:  a function that concatenates two strings
+ *
+ * Return: NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int x = 0, y = 0, len = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[x])
+	{
+		len++;
+		x++;
+	}
+	x = 0;
+	while (s2[x])
+	{
+		s[x] = s1[x];
+		x++;
+	}
+	s[x] = '\0';
+
+	return (s);
+}
